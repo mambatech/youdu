b4b8f8b511ca0b324084fd936455fe0457edc42a
liaoshouwang <Edison.liao@cootek.cn>
2020-03-09 00:18:19 +0800

    项目结构调整

--- app/src/main/java/com/duyin/bookshelf/helper/PrivacyAndUserLienceHelper.kt
+++ app/src/main/java/com/duyin/bookshelf/helper/PrivacyAndUserLienceHelper.kt
@@ -11,20 +11,20 @@ created by edison 2020-02-27
  */
 object PrivacyAndUserLienceHelper {
 
-    fun gotoPrivacyActivity(context: Context) {
+    fun gotoPrivacyActivity(context: Context?) {
         val intent = Intent(context, CommonWebActivity::class.java)
         intent.putExtra(CommonWebActivity.KEY_URL, AppConstant.PRIVACY_POLICY_OTHER_URL)
-        intent.putExtra(CommonWebActivity.KEY_TITLE, context.getString(R.string.privacy_title))
+        intent.putExtra(CommonWebActivity.KEY_TITLE, context?.getString(R.string.privacy_title))
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context.startActivity(intent)
+        context?.startActivity(intent)
     }
 
-    fun gotoUserLicenseActivity(context: Context) {
+    fun gotoUserLicenseActivity(context: Context?) {
         val intent = Intent(context, CommonWebActivity::class.java)
         intent.putExtra(CommonWebActivity.KEY_URL, AppConstant.USER_LICENSE_OTHER_URL)
-        intent.putExtra(CommonWebActivity.KEY_TITLE, context.getString(R.string.user_license_title))
+        intent.putExtra(CommonWebActivity.KEY_TITLE, context?.getString(R.string.user_license_title))
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context.startActivity(intent)
+        context?.startActivity(intent)
     }
 
 }
